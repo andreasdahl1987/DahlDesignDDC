2b9ead9ef70aac3966745916eec0b2a4b9e8d5c6
andreasdahl1987 <40788634+andreasdahl1987@users.noreply.github.com>
2022-08-19 00:07:20 +0200

    Latest edit

--- DahlDesignDDC/63_RotaryPEC11.ino
+++ DahlDesignDDC/63_RotaryPEC11.ino
@@ -6,14 +6,17 @@ void PEC11(int row, int col, bool reverse) {
 
     int Reverse = reverse;
 
-    if (!rawState[Row][Column] && !rawState[Row][Column+1])
+    if (!rawState[Row][Column] && !rawState[Row][Column + 1])
     {
         pushState[Row][Column] = 1;
     }
     else if (!rawState[Row][Column] && rawState[Row][Column + 1])
     {
         pushState[Row][Column] = 2;
-        latchLock[Row][Column] = 1; //Fetching 01
+        if ((globalClock - switchTimer[Row][Column] > PEC11Cooldown) && (globalClock - switchTimer[Row][Column + 1] > PEC11Cooldown))
+        {
+            latchLock[Row][Column] = 1; //Fetching 01 only if we're not cooling down
+        }
     }
     else if (rawState[Row][Column] && rawState[Row][Column + 1])
     {
@@ -22,7 +25,11 @@ void PEC11(int row, int col, bool reverse) {
     else if (rawState[Row][Column] && !rawState[Row][Column + 1])
     {
         pushState[Row][Column] = 4;
-        latchLock[Row][Column + 1] = 1; //Fetching 10
+        if ((globalClock - switchTimer[Row][Column] > PEC11Cooldown) && (globalClock - switchTimer[Row][Column + 1] > PEC11Cooldown))
+        {
+            latchLock[Row][Column + 1] = 1; //Fetching 10 only if we're not cooling down
+        }
+
     }
 
     if ((globalClock - switchTimer[Row][Column] > PEC11Cooldown) && (globalClock - switchTimer[Row][Column + 1] > PEC11Cooldown))
@@ -42,13 +49,6 @@ void PEC11(int row, int col, bool reverse) {
         }
     }
 
-    else
-    {
-        latchLock[Row][Column + 1] = 0;
-        latchLock[Row][Column] = 0;
-    }
-
     Joystick.setButton(Number + Reverse, (globalClock - switchTimer[Row][Column] < PEC11Pulse));
     Joystick.setButton(Number + 1 - Reverse, (globalClock - switchTimer[Row][Column + 1] < PEC11Pulse));
-
 }
