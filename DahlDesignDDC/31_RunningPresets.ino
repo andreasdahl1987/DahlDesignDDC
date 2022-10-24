ac78f3a4fecec6d4ce2afb478a320caf63a803d5
Andreas Dahl <40788634+andreasdahl1987@users.noreply.github.com>
2022-10-24 11:51:56 +0200

    Update 31_RunningPresets.ino

--- DahlDesignDDC/31_RunningPresets.ino
+++ DahlDesignDDC/31_RunningPresets.ino
@@ -5,10 +5,10 @@ void runningPresets()
     happens once after you switch preset. This is great for static stuf like switch modes, bite point, etc. But if you set a bite point here, you won't
     be able to change it with your rotary, since it will continously be updated to the value you set here. 
 
-    This area is for changing major things like button numbers, switch function assignments, etc. If you want your 12-way to have a regular 12-position
-    mode in ACC, but use a multiComplex24 function (which includes other pushbuttons) in another preset, this is where you set it. You might want to 
-    use a HybridSwitch in one preset, and since hybrid buttons dont produce a button number, this button is lost in other presets that are meant for 
-    sims that dont support this kind of switch. You could then use runningPresets() to set hybridButton(1,2); in all presets except the one you use
+    This area is mainly for changing switch function assignments. If you want your 12-way to have a regular 12-position mode in ACC, but use a multiComplex24 
+    function (which includes other pushbuttons) in another preset, this is where you set it. You might want to use a HybridSwitch in one preset, 
+    and since hybrid buttons dont produce a button number, this button is lost in other presets that are meant for sims that dont support 
+    this kind of switch. You could then use runningPresets() to set hybridButton(1,2); in all presets except the one you use
     for F1 2022, where it would be pushButton(1,2);. Just examples, use your imagination. Setting up parts of your controller in this function can make 
     your controller very flexible.    
     */
