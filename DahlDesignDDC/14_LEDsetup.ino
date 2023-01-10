9e020524a9a89010e010d23f443422bb0aed8d94
andreasdahl1987 <40788634+andreasdahl1987@users.noreply.github.com>
2023-01-10 22:12:28 +0100

    Edit

--- DahlDesignDDC/14_LEDsetup.ino
+++ DahlDesignDDC/14_LEDsetup.ino
@@ -5,7 +5,7 @@
 // How many leds on this strip? Set to 0 to disable.
 #define STRIP1_RGBLEDCOUNT 0
 // Data pin
-#define STRIP1_DATAPIN 3
+#define STRIP1_DATAPIN 2
 // 0 left to right, 1 right to left
 #define STRIP1_RIGHTTOLEFT 0
 // Strip type (NEOPIXEL = WS2812B compatible, PL9823 = PL9823)
@@ -15,7 +15,7 @@
 // ------- STRIP #2 Settings
 //-------------------------
 #define STRIP2_RGBLEDCOUNT 0
-#define STRIP2_DATAPIN 7 
+#define STRIP2_DATAPIN 2 
 #define STRIP2_RIGHTTOLEFT 0
 #define STRIP2_TYPE NEOPIXEL
 
@@ -23,7 +23,7 @@
 // ------- STRIP #3 Settings
 //-------------------------
 #define STRIP3_RGBLEDCOUNT 0
-#define STRIP3_DATAPIN 8
+#define STRIP3_DATAPIN 2
 #define STRIP3_RIGHTTOLEFT 0
 #define STRIP3_TYPE NEOPIXEL
 
@@ -32,7 +32,7 @@
 //-------------------------
 
 #define STRIP4_RGBLEDCOUNT 0
-#define STRIP4_DATAPIN 9
+#define STRIP4_DATAPIN 2
 #define STRIP4_RIGHTTOLEFT 0
 #define STRIP4_TYPE NEOPIXEL
 
