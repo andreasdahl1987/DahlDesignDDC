1e1c065eef50f982a085e228e1b9f2a3ac7f79c5
Andreas Dahl <40788634+andreasdahl1987@users.noreply.github.com>
2023-01-18 23:19:58 +0100

    Edit documentation

--- DahlDesignDDC/11_ShiftRegisters.ino
+++ DahlDesignDDC/11_ShiftRegisters.ino
@@ -2,25 +2,25 @@
 
 #define SRCOUNT 0
 
-//Shift register cluster 1
+//Shift register chain 1
 #define SR1CHAIN 1
 #define SR1DATA 2
 #define SR1CLOCK 3
 #define SR1LATCH 4
 
-//Shift register cluster 2
+//Shift register chain 2
 #define SR2CHAIN 1
 #define SR2DATA 2
 #define SR2CLOCK 3
 #define SR2LATCH 4
 
-//Shift register cluster 3
+//Shift register chain 3
 #define SR3CHAIN 1
 #define SR3DATA 2
 #define SR3CLOCK 3
 #define SR3LATCH 4
 
-//Shift register cluster 4
+//Shift register chain 4
 #define SR4CHAIN 1
 #define SR4DATA 2
 #define SR4CLOCK 3
