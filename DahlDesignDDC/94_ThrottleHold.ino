5fe4c7109e14e176920ddf11381a74030a28354f
Andreas Dahl <40788634+andreasdahl1987@users.noreply.github.com>
2023-02-01 23:24:57 +0100

    Added throtleHoldSW1 and fixed max/min overshoot on throttle hold and brake magic adjustment

--- DahlDesignDDC/94_ThrottleHold.ino
+++ DahlDesignDDC/94_ThrottleHold.ino
@@ -1,3 +1,124 @@
+void throttleHoldSW1(int8_t buttonRow, int8_t buttonCol, int8_t rotaryRow, int8_t rotaryColA, int rotaryColB, bool reverse)
+{
+    int8_t ButtonRow = buttonRow - 1;
+    int8_t ButtonCol = buttonCol - 1;
+    int Row = rotaryRow - 1;
+    int Column = rotaryColA - 1;
+    int Number = buttonNumber[rotaryRow][rotaryColA];
+
+    int bCol = rotaryColB - 1;
+    
+    int8_t Reverse = reverse;
+
+    //Button logics
+    if (pushState[ButtonRow][ButtonCol] != rawState[ButtonRow][ButtonCol] && (globalClock - switchTimer[ButtonRow][ButtonCol]) > buttonCooldown)
+    {
+        switchTimer[ButtonRow][ButtonCol] = globalClock;
+        pushState[ButtonRow][ButtonCol] = rawState[ButtonRow][ButtonCol];
+    }
+
+    if ((globalClock - switchTimer[ButtonRow][ButtonCol]) > buttonCooldown)
+    {
+        pushState[ButtonRow][ButtonCol] = rawState[ButtonRow][ButtonCol];
+    }
+
+
+    //Encoder logics
+    if (!rawState[Row][Column] && !rawState[Row][bCol])
+    {
+        pushState[Row][Column] = 1;
+    }
+    else if (!rawState[Row][Column] && rawState[Row][bCol])
+    {
+        pushState[Row][Column] = 2;
+        latchLock[Row][Column] = 1; //Fetching 01
+    }
+    else if (rawState[Row][Column] && rawState[Row][bCol])
+    {
+        pushState[Row][Column] = 3;
+    }
+    else if (rawState[Row][Column] && !rawState[Row][bCol])
+    {
+        pushState[Row][Column] = 4;
+        latchLock[Row][bCol] = 1; //Fetching 10
+    }
+
+    if ((globalClock - switchTimer[Row][Column] > funkyCooldown) && (globalClock - switchTimer[Row][bCol] > funkyCooldown))
+    {
+        if ((latchLock[Row][bCol] && pushState[Row][Column] == 1) || (latchLock[Row][Column] && pushState[Row][Column] == 3))
+        {
+            switchTimer[Row][Column] = globalClock;
+
+            if (brakeMagicOn)
+            {
+                brakeMagicValue = brakeMagicValue + 10 - (20 * Reverse);
+            }
+            else if (pushState[ButtonRow][ButtonCol] == 1)
+            {
+                throttleHoldValue = throttleHoldValue + 10 - (20 * Reverse);
+            }
+        }
+
+        else if ((latchLock[Row][bCol] && pushState[Row][Column] == 3) || (latchLock[Row][Column] && pushState[Row][Column] == 1))
+        {
+            switchTimer[Row][bCol] = globalClock;
+            if (brakeMagicOn)
+            {
+                brakeMagicValue = brakeMagicValue - 10 + (20 * Reverse);
+            }
+            else if (pushState[ButtonRow][ButtonCol] == 1)
+            {
+                throttleHoldValue = throttleHoldValue - 10 + (20 * Reverse);
+            }
+            
+        }
+    }
+
+    else
+    {
+        latchLock[Row][bCol] = 0;
+        latchLock[Row][Column] = 0;
+    }
+    if (!brakeMagicOn && pushState[ButtonRow][ButtonCol] == 0)
+    {
+      Joystick.setButton(Number + Reverse, (globalClock - switchTimer[Row][Column] < funkyPulse));
+      Joystick.setButton(Number + 1 - Reverse, (globalClock - switchTimer[Row][bCol] < funkyPulse));
+    }
+
+    //Adjusting 
+    if (brakeMagicValue < 0)
+    {
+      brakeMagicValue = 0;
+    }
+    else if (brakeMagicValue > 1000)
+    {
+      brakeMagicValue = 1000;
+    }
+
+    if (throttleHoldValue < 0)
+    {
+      throttleHoldValue = 0;
+    }
+    else if (throttleHoldValue > 1000)
+    {
+      throttleHoldValue = 1000;
+    }
+
+    
+    if (pushState[ButtonRow][ButtonCol] == 1)
+    {
+        Joystick.setThrottle(throttleHoldValue);
+    }
+    else
+    {
+        Joystick.setThrottle(0);
+    }
+
+    long push = pushState[ButtonRow][ButtonCol];
+    push = push << 5;
+    buttonField = buttonField | push;
+}
+
 void throttleHoldT(int8_t buttonRow, int8_t buttonCol, int8_t rotaryRow, int8_t rotaryCol, bool reverse)
 {
     int8_t ButtonRow = buttonRow - 1;
@@ -87,6 +208,25 @@ void throttleHoldT(int8_t buttonRow, int8_t buttonCol, int8_t rotaryRow, int8_t 
                 }
             }
         }
+    }
+
+    //Adjusting 
+    if (brakeMagicValue < 0)
+    {
+      brakeMagicValue = 0;
+    }
+    else if (brakeMagicValue > 1000)
+    {
+      brakeMagicValue = 1000;
+    }
+
+    if (throttleHoldValue < 0)
+    {
+      throttleHoldValue = 0;
+    }
+    else if (throttleHoldValue > 1000)
+    {
+      throttleHoldValue = 1000;
     }
 
     int8_t difference = pushState[Row][Column + 1];
@@ -233,6 +373,25 @@ void throttleHoldM(int8_t buttonRow, int8_t buttonCol, int8_t rotaryRow, int8_t 
         }
     }
 
+    //Adjusting 
+    if (brakeMagicValue < 0)
+    {
+      brakeMagicValue = 0;
+    }
+    else if (brakeMagicValue > 1000)
+    {
+      brakeMagicValue = 1000;
+    }
+
+    if (throttleHoldValue < 0)
+    {
+      throttleHoldValue = 0;
+    }
+    else if (throttleHoldValue > 1000)
+    {
+      throttleHoldValue = 1000;
+    }
+
     int8_t difference = pushState[Row][Column + 1];
     if (difference != 0 && !brakeMagicOn && !latchState[ButtonRow][ButtonCol])
     {
@@ -275,4 +434,3 @@ void throttleHoldM(int8_t buttonRow, int8_t buttonCol, int8_t rotaryRow, int8_t 
     push = push << 5;
     buttonField = buttonField | push;
 }
-
