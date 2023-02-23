15b8e7eaf2c0c65c27a2a08c959a1973f3070fc0
Andreas Dahl <40788634+andreasdahl1987@users.noreply.github.com>
2023-02-23 01:17:20 +0100

    Update 01_ColorPalette.ino

--- DahlDesignDDC/01_ColorPalette.ino
+++ DahlDesignDDC/01_ColorPalette.ino
@@ -1,12 +1,5 @@
 /*
- * Any 24-bit color is possible with RGB LEDs, but here are the common 140 browser colors listed with their hexadecimal values.
+ * Any 24-bit color is possible with RGB LEDs, but here will be listed the common 140 browser colors listed with their hexadecimal values.
  * 
- * For better overview, have a look at this page: https://htmlcolorcodes.com/color-names/
+ * For now, have a look at this page: https://htmlcolorcodes.com/color-names/
  */
-
-//------------------------------
-//--------RED COLORS------------
-//------------------------------
-
-//Indian Red ------------ | 0xCD5C5C
-//Light Coral ----------- | 0xF08080
