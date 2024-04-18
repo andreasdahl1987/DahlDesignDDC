76efcfb1dd1bec1b1f35836abf1114b7eb0d1925
Andreas Dahl <40788634+andreasdahl1987@users.noreply.github.com>
2024-04-18 22:51:25 +0200

    tidy up

--- DahlDesignDDC/09_CB1_Table.ino
+++ DahlDesignDDC/09_CB1_Table.ino
@@ -35,7 +35,7 @@ uint8_t buttonNumber[rowCount][colCount] =
 
 #define analogChannelCount 12
 
-uint8_t analogPins[analogChannelCount] =                 //Pin number for your analog channels
+uint8_t analogPins[analogChannelCount] =              
 { ADC1, ADC2, ADC3, ADC4, ADC5, ADC6, ADC7, ADC8, ADC9, ADC10, ADC11, ADC12 };
 
 uint8_t analogButtonNumber[analogChannelCount] =         //ANALOG BUTTONS 1
--- DahlDesignDDC/10_TableAndAnalog.ino
+++ DahlDesignDDC/10_TableAndAnalog.ino
@@ -4,10 +4,10 @@
 //------------TABLE VARIABLES-------------
 //-----------------------------------------
 
-uint8_t row[] = { 99};
+uint8_t row[] = { 0, 0, 0, 0, 0};
 const uint8_t rowCount = sizeof(row) / sizeof(row[0]);
 
-uint8_t col[] = { 99,99,99,99};
+uint8_t col[] = { 0, 0, 0, 0, 0};
 const uint8_t colCount = sizeof(col) / sizeof(col[0]);
 
 //---------------------------------------
@@ -16,8 +16,11 @@ const uint8_t colCount = sizeof(col) / sizeof(col[0]);
 
 uint8_t buttonNumber[rowCount][colCount] =
 {
-  {0, 1, 2, 3}    //ROW1
-
+  {0, 0, 0, 0, 0},    //ROW1
+  {0, 0, 0, 0, 0},    //ROW2
+  {0, 0, 0, 0, 0},    //ROW3
+  {0, 0, 0, 0, 0},    //ROW4
+  {0, 0, 0, 0, 0}     //ROW5
 };
 
 //---------------------------------------
@@ -25,7 +28,7 @@ uint8_t buttonNumber[rowCount][colCount] =
 //---------------------------------------
 
 
-uint8_t directPins[] = { 21,27,26,22 };
+uint8_t directPins[] = { 99 };
 const uint8_t directPinsCount = sizeof(directPins) / sizeof(directPins[0]);
 
 
@@ -36,7 +39,7 @@ const uint8_t directPinsCount = sizeof(directPins) / sizeof(directPins[0]);
 #define analogChannelCount 1                             //Number of analog channels
 
 uint8_t analogPins[analogChannelCount] =                 //Pin number for your analog channels
-{ A2 };
+{ 99 };
 
 uint8_t analogButtonNumber[analogChannelCount] =         //Button number for your analog channels
 { 0 };
