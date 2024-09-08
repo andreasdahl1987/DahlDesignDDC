5d1aa69ba72149d0a30d60f11ef322bf269d51d9
Andreas Dahl <40788634+andreasdahl1987@users.noreply.github.com>
2024-09-08 15:43:28 +0200

    Cleanup

--- DahlDesignDDC/04_USB.ino
+++ DahlDesignDDC/04_USB.ino
@@ -1,6 +1,6 @@
 //This only relevant for RP2040 LED support
 
 #define VID_PI 0x2E8A
-#define PID_PI 0x1051
-#define MAKER "SOELPEC"
-#define CONTROLLER_NAME "SOELPEC Spectra XR"
+#define PID_PI 0x1053
+#define MAKER "Andreas Dahl"
+#define CONTROLLER_NAME "Dahl Design Controller"
